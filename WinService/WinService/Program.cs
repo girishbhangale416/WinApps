cffc9193421db76d0edd1803331e4fa40f80fe5b
praba1986 <42798803+praba1986@users.noreply.github.com>
2018-11-08 17:21:16 +0800

    comment added

--- WinService/WinService/Program.cs
+++ WinService/WinService/Program.cs
@@ -14,6 +14,7 @@ namespace WinService
         /// </summary>
         static void Main()
         {
+            //service started
             ServiceBase[] ServicesToRun;
             ServicesToRun = new ServiceBase[]
             {
